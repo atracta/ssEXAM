667691244972a475f2559fece4f078014ab8757c
atracta <aatractaa@protonmail.ch>
2023-10-22 20:18:53 -0500

    update commit

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -1,8 +1,13 @@
+# This script creates a user with root permissions, installs Apache, MySQL, PHP, and Laravel, and configures Apache to serve Laravel if needed.
+# It takes one argument, which is the node type (master or slave).
+# If the script is not run with root permissions, it will exit with an error message.
+# If the node type is master, it will create a user with root permissions, install Apache, MySQL, PHP, and Laravel, and configure Apache to serve Laravel.
+# If the node type is slave, it will print a message and exit.
 #!/bin/bash
 NODE_TYPE=$1
 # Ensure the script is run with root permissions
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or with sudo."
+  echo “Please run as root or with sudo.”
   exit
 fi
 # Common Process and Softwares
@@ -10,6 +15,7 @@ apt-get update
 # Give user root permissions for master
 if [ "$NODE_TYPE" == "master" ]; then
     # create user
+    echo "######### Create Master User ##########"
     adduser master --disabled-password --gecos ""
     echo "master:password" | chpasswd
     # add root permissions
@@ -21,26 +27,28 @@ if [ "$NODE_TYPE" == "master" ]; then
     sudo apt-get update
     sudo apt-get upgrade -y
     sudo apt-get install -y apache2 software-properties-common
-
-    sudo systemctl enable 
+    sudo systemctl enable apache2
     sudo systemctl restart apache2
-    # Clone Laravel and install its dependencies
-    sudo add-apt-repository ppa:ondrej/php -y
-    sudo apt update
-    sudo apt install php7.4 libapache2-mod-php php-mbstring php-cli php-bcmath php-json php-xml php-zip php-pdo php-common php-tokenizer php-mysql mysql-server
-
+    # Clone Laravel and install its dependenciesp
+    sudo apt install php libapache2-mod-php php-xml php-curl php-mbstring php-xmlrpc php-soap php-gd php-xml php-cli php-zip php-bcmath php-tokenizer php-json php-pear
+    sudo apt install php-cli php-mysql mysql-server curl -y
+    cd ~
+    curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+    HASH=`curl -sS https://composer.github.io/installer.sig`
+    echo $HASH
+    php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
     echo "mysql-server mysql-server/root_password password rootpass" | debconf-set-selections
     echo "mysql-server mysql-server/root_password_again password rootpass" | debconf-set-selections
     php -v
     sudo git clone https://github.com/laravel/laravel.git /var/www/laravel
     cd /var/www/laravel
-    sudo -u master composer install
-    sudo -u master cp .env.example .env
-    sudo -u master php artisan key:generate
-    sudo -u master php artisan migrate
-    sudo -u master php artisan db:seed
-    # Configure Apache to serve Laravel if needed
-    # Install MySQL server and secure it
-elif [ "$NODE_TYPE" == "slave" ]; then
-   echo "thank you"
+    sudo a2enmod rewrite
+    systemctl restart apache2
+    sudo chown  -R www-data:www-data /var/www/laravel
+    sudo composer install --ignore-platform-req=ext-curl
+    sudo -u master cp .env.example /var/www/laravel/.env
+    # sudo -u master php artisan key:generate
+    # sudo -u master php artisan migrate
+    # sudo -u master php artisan db:seed
 fi
